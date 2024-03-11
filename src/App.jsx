6eae9482334a7a8434154eb13e9f81ce23830fb4
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-03-11 04:14:12 -0700

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,44 @@ import Home from './Home'
 function App() {
   const navigate=useNavigate()
   const {  handleSubmit,register,formState: { errors }} = useForm()
+  
   const onSubmit=(data)=>{
-    if(data.passworduser !== data.passwordconfirm){
-      toast.error(" passwords do not match")
+    
+     if(data.nomUser.length ==""){
+     toast.error("Completez le nom SVP!!")
     }
+     else if(data.passworduser.length ==""){
+    toast.error("veiller saisir le mot de pass")
+   }
+  
     else{
-
-
       toast.success("Successful connection")
       navigate("/home")
     }
   }  
-
+  
   return (
       <main>
     <form onSubmit={handleSubmit(onSubmit)}>
-      
+        <h1>Log in to your acount</h1>
+        <p ></p>
         <input type="text" 
         placeholder="Enter your name" 
-        {...register("nomUser",{required:"please enter your name",
+        
+        {...register("nomUser",{
         minLength:{value:2,message:"your name must have at least 2 characters"}})}
         />
-        <input type="email" placeholder="Enter your email address" 
-        {...register("adressmail",{required:"make sure to enter the email address",
-        pattern: {
-          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-          message: 'Invalid email address'
-        }
-      })}
-        />
-        <input type="password" placeholder="Enter your password"  
-        {...register("passworduser",{required:"make sure you enter your password",
-        minLength:{value:5,message:"your password must have at least 5 characters"}})}/>
-        <input type="password" placeholder="Confirm password"  
-        {...register("passwordconfirm",{required:"make sure to confirm your password",
-        minLength:{value:5,message:"your password must have at least 5 characters"}})}/>
+        {errors.nomUser && (
+        <span style={{ color: 'red' }}>{errors.nomUser.message}</span>
+      )}
+        
+        <input type="password" 
+        placeholder="Enter your password"  
+        {...register("passworduser",{
+        minLength:{value:5,message:"identifient incorrect"}})}/>
+      {errors.passworduser && (
+        <span style={{ color: 'red' }}>{errors.passworduser.message}</span>
+      )}
         <button type="submit">Login</button>
     </form>
      </main>
